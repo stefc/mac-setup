4fa626b7e461b46780b48f5f942d4bc869270448
Stefc <stefan.boether@gmail.com>
2025-12-11 10:52:14 +0100

    close.mit cmd+W added

--- config/.wezterm.lua
+++ config/.wezterm.lua
@@ -7,5 +7,12 @@ config.color_scheme = 'Selenized Dark (Gogh)'
 
 config.font_size = 16
 config.font = wezterm.font('JetBrains Mono')
+config.keys = {
+  {
+    key = 'w',
+    mods = 'SUPER',
+    action = wezterm.action.CloseCurrentPane { confirm = true },
+  },
+}
 
 return config
