99172507bcf95e61347b4d21049c59a610b6489a
Stefc <stefan.boether@gmail.com>
2026-03-22 20:49:07 +0100

    Add thiserror dependency and refactor SetupError for improved error handling

--- src/common/error.rs
+++ src/common/error.rs
@@ -1,36 +1,12 @@
-use std::error::Error as StdError;
-use std::fmt;
+use std::io::Error as IoError;
+use thiserror::Error;
 
-#[derive(Debug)]
+#[derive(Error,Debug)]
 pub enum SetupError {
+    #[error("Command failed: '{command}' (exit code: {exit_code:?})")]
     CommandFailed { command: String, exit_code: Option<i32> },
-    Io(std::io::Error),
-}
-
-impl fmt::Display for SetupError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            SetupError::CommandFailed { command, exit_code } => {
-                write!(f, "Command failed: '{}' (exit code: {:?})", command, exit_code)
-            }
-            SetupError::Io(e) => write!(f, "IO error: {}", e),
-        }
-    }
-}
-
-impl StdError for SetupError {
-    fn source(&self) -> Option<&(dyn StdError + 'static)> {
-        match self {
-            SetupError::Io(e) => Some(e),
-            _ => None,
-        }
-    }
-}
-
-impl From<std::io::Error> for SetupError {
-    fn from(err: std::io::Error) -> Self {
-        SetupError::Io(err)
-    }
+    #[error("IO error: {0}")]
+    Io(IoError),
 }
 
 pub type SetupResult<T> = Result<T, SetupError>;
