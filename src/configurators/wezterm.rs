979200567d5d142d80fc4818d1bf76d570a2081f
Stefc <stefan.boether@gmail.com>
2026-06-23 17:15:12 +0200

    refactoring wezterm configurator

--- src/configurators/wezterm.rs
+++ src/configurators/wezterm.rs
@@ -6,39 +6,28 @@ use crate::symlinks::SetupResult;
 
 pub struct WeztermConfigurator;
 
-impl WeztermConfigurator {
-    fn is_installed(&self) -> bool {
-        WezTermDetector.is_installed()
-    }
-
-    fn run_configure(&self, logger: &mut dyn Log) -> SetupResult<()> {
-        // Ensure `code` CLI is available. If not, we try to proceed but will error when executing.
-        if !self.is_installed() {
-            return Ok(());
-        }
-
-        Ok(())
-    }
-}
-
 impl Configurator for WeztermConfigurator {
     fn name(&self) -> &'static str {
         "Wezterm"
     }
 
     fn should_run(&self) -> bool {
-        if !self.is_installed() {
+        if !WezTermDetector.is_installed() {
             return false;
         }
         true
     }
 
     fn configure(&self, logger: &mut dyn Log) -> SetupResult<()> {
-        self.run_configure(logger)
+
+        if !WezTermDetector.is_installed() {
+            return Ok(());
+        }
+
+        Ok(())
     }
 
     fn affected_files(&self) -> Vec<String> {
-        // VS Code changes user extensions; no specific file path returned here
         Vec::new()
     }
 }
